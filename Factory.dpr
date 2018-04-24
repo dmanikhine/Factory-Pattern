4123c043337009bf773dedbca102025abcb7f4e8
dmanikhine <38524402+dmanikhine@users.noreply.github.com>
2018-04-24 15:14:12 +0200

    Dritter Schritt

--- Factory.dpr
+++ Factory.dpr
@@ -6,8 +6,20 @@ program Factory;
 
 uses
   System.SysUtils,
-  uTSimplePizza in 'uTSimplePizza.pas';
+  uTSimplePizza in 'uTSimplePizza.pas',
+  uTPizzaStore in 'uTPizzaStore.pas';
 
+ procedure DoPizzaStore;
+ var PizzaStore:  TPizzaStore;
+ begin
+   PizzaStore:=TPizzaStore.Create;
+   try
+   PizzaStore.OrderPizza("cheese");
+
+   end;
+   finally
+    PizzaStore.Free;
+ end;
 begin
   try
     { TODO -oUser -cConsole Main : Code hier einfügen }
